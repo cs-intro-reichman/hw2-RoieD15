5ca7e2924c05453056b57f120926862d72ae3a45
Roie Drukman <roie.drukman@post.runi.ac.il>
2024-11-16 20:46:55 +0200

    done

--- CalcPi.java
+++ CalcPi.java
@@ -1,6 +1,18 @@
 // Computes an approximation of PI.
 public class CalcPi {
 	public static void main(String [] args) { 
-	    // Replace this comment with your code
+	    System.out.println("pi according to Java: " + Math.PI);
+		int terms = Integer.parseInt(args[0]);
+		double approximatePi = 1.0;
+		int divider = 3;
+		for(int i = 0; i < terms; i++) {
+			if(i % 2 == 0) {
+				approximatePi = approximatePi - 1 / divider;
+			} else {
+				approximatePi = approximatePi + 1 / divider;
+			}
+			divider = divider + 2;
+		}
+		System.out.println("pi, approximated:     " + approximatePi);
 	}
 }
--- Collatz.java
+++ Collatz.java
@@ -1,23 +1,28 @@
 // Demonstrates the Collatz conjecture.
 public class Collatz {
 	public static void main(String args[]) {
+		int top = Integer.parseInt(args[0]);
 		int count = 0;
-		int seed = 1;
-		int lastChar = 0;
-		for(int i = 0; i < Integer.parseInt(args[0]); i++) {
-			System.out.print(seed + " ");
-			while(lastChar != 1) {
-			if(lastChar % 2 == 0) {
-				lastChar = seed / 2;
-			} else {
-				lastChar = (seed * 3) + 1;
+		int seed = 0;
+		for(int i = 0; i < top; i++) {
+			System.out.print(i + " ");
+			while(seed != 1) {
+				seed = i;
+				if(seed % 2 == 0) {
+					seed = seed / 2;
+					System.out.println("(" + seed + ")");
+					count++;
+				} else {
+					seed = (seed * 3) + 1;
+					System.out.println("(" + seed + ")");
+					count++;
+				}
 			}
-			System.out.print(lastChar + " ");
-			count++;
+			System.out.println("(" + count + ")");
 			seed++;
 		}
-		System.out.println("(" + count + ")");
-		}
-		System.out.println("Every one of the first " + args[0] + "hailstone sequences reached 1.");
+		System.out.println("Every one of the first " + top + " hailstone sequences reached 1.");
+	
 	}
-}
+	}
+	
