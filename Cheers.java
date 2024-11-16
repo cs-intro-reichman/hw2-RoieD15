5be5fd1967e6d6446b92964ab1925b6dfc6bfea1
Roie Drukman <roie.drukman@post.runi.ac.il>
2024-11-16 17:29:05 +0200

    donw item 3

--- Cheers.java
+++ Cheers.java
@@ -1,6 +1,18 @@
 // Prints a crowd cheering output.
 public class Cheers {
         public static void main(String[] args) {
-	    // Replace this comment with your code
+	    String word = args[0].toUpperCase();
+            String specialLettes = "AEFHILMNORSX";
+            for(int i = 0 ; i < word.length() ; i++){
+                char letter = word.charAt(i);
+                for(int j = 0; j < specialLettes.length(); j++) {
+                        if(specialLettes.charAt(j) == letter){
+                                System.out.println("Give me an " + letter + ":" + letter + "!");
+                        } else {
+                                System.out.println("Give me a " + letter + ":" + letter + "!");
+                        }
+                }
+                
+            }
         }
 }
