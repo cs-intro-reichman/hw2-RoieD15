10b9425cd9723f39a134ef540c6c1d7fad17c8d0
Roie Drukman <roie.drukman@post.runi.ac.il>
2024-11-17 10:29:12 +0200

    done item 5

--- Collatz.java
+++ Collatz.java
@@ -2,29 +2,35 @@
 public class Collatz {
 	public static void main(String args[]) {
 		int top = Integer.parseInt(args[0]);
-		int seed = 1;
-		int count = 0;
-		int lastNum = 0;
-		for(int i = 1; i < top+1; i++)
-		{
+		char mode = args[1].charAt(0);
+		if(mode == 'v') {
+			for(int i = 1; i < top+1; i++) {
 			System.out.print(i + " ");
+			int seed = i;
+			int count = 1;
+			int lastNum = 0;
 			while(lastNum != 1) {
-				count++;
 				if(seed % 2 ==0) {
 					seed = seed / 2;
 					lastNum = seed;
-					System.out.println(seed);
+					count++;
+					System.out.print(seed + " ");
 				} else {
 					seed = (seed * 3) + 1;
 					lastNum = seed;
-					System.out.println(seed);
+					count++;
+					System.out.print(seed + " ");
 				}
 			}		
-			System.out.println("(" + count + ")");
-			System.out.print("Every one of the first " + top + " hailstone sequences reached 1.");	
+			System.out.println("(" + count + ")");	
 		}
-	}	
-	
+		System.out.print("Every one of the first " + top + " hailstone sequences reached 1.");
+		} else {
+			System.out.print("Every one of the first " + top + " hailstone sequences reached 1.");
+		}
 	}
+}	
+	
+	
 	
 	
